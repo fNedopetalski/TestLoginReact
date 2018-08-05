b1288f98602af82489d736dd7d119e04c4712687
Felipe Nedopetalski <fnedopetalski@gmail.com>
2018-08-04 21:25:34 -0300

    .

--- screens/Salas/CriarSala.js
+++ screens/Salas/CriarSala.js
@@ -10,11 +10,49 @@ export default class CriarSala extends React.Component{
 
     render() {
         return (
-            <View style={{padding:20}}>
-                <Text>CriarSalaScreen</Text>
-                <View style={{paddingTop:15}}/>
-                <Button title="Voltar" onPress={this.onVoltarPress}/>
+            <View style={styles.container}>
+                <View style={styles.topo}>
+                    <Button title="Voltar" onPress={this.onVoltarPress}/>
+                </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        backgroundColor: '#333',
+    },
+
+    topo: {
+        padding:30,
+        marginBottom: 10,
+        alignItems: 'flex-end',
+        backgroundColor: '#000',
+    },
+
+    welcome: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginTop: 5,
+    },
+
+    login: {
+        alignSelf: 'stretch',
+        marginTop: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderWidth: 1,
+        borderColor: '#DDD',
+        height: 40,
+        borderRadius: 3,
+    },
+
+    buttonContainer:{
+        marginTop: 15,
+        height: 150,
+    },
+
+});
